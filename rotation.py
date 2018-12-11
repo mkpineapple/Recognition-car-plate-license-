3f7579dba2dbe7690febb1573bd7997b1f71b3d0
mkpineapple <38205063+mkpineapple@users.noreply.github.com>
2018-12-10 21:40:43 -0600

    Create rotation.py

---
+++ rotation.py
@@ -0,0 +1,19 @@
+#-*- coding: UTF-8 -*-
+# @author Ke Ma for final project
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+
+file_dir = os.getcwd()
+for root, dirs, files in os.walk(file_dir):
+    file_list = files
+
+for photo in files:
+    x = photo.split('-')
+    if len(x) > 1:
+        im = Image.open(photo)
+        # im.show(100)
+
+        im_rotate = im.rotate(15)
+        # im_rotate.show(100)
+        im_rotate.save(photo, quality=100, optimize=True)
